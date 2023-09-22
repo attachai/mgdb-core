a8ab448322aae689fec5076f6b01ae635e058f2e
attachai@ianalysis.biz <attachai@ianalysis.biz>
2023-09-22 08:45:02 +0700

    update variable

--- app/const/env_variable.go
+++ app/const/env_variable.go
@@ -6,4 +6,9 @@ const (
 	LogLevel      = "logLevel"
 	ServerLogging = "ServerLogging"
 	OnServerLog   = "OnServerLog"
+	Mode          = "mode"
+	Prod          = "prod"
+	FusionHost    = "fusionHost"
+	ApiKey        = "apiKey"
+	HTTPPort      = "HTTPPort"
 )
